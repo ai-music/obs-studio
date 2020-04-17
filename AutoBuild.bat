60fde94e923af48ffe4171f9f99b3ec550cbd153
RobCoolBlue <rob@coolbluesoftware.com>
2020-04-17 17:42:39 +0100

    Updated tile PNGs, added JPGs for installer. Updated AIP file

--- AutoBuild.bat
+++ AutoBuild.bat
@@ -1,6 +1,7 @@
 git clone --recursive https://github.com/ai-music/obs-studio.git
 
 cd obs-studio
+mkdir build64
 
 SET branchName=%1
 IF NOT DEFINED branchName SET branchName="Master"
@@ -8,11 +9,14 @@ IF NOT DEFINED branchName SET branchName="Master"
 echo "Checking out branch "%branchName%
 git checkout %branchName%
 
-cmake -DDepsPath:STRING=C:/OBS/Deps/win64
-cmake -DQTDIR:STRING=C:/Qt/5.14.2/msvc2017_64
-cmake -S . -B build64
+git tag > tags.txt
 
+cmake -DDepsPath:STRING=C:/OBS/Deps/win64 -DQTDIR:STRING=C:/Qt/5.14.2/msvc2017_64 -S C:/OBS/obs-studio -B C:/OBS/obs-studio/build64
 
+MSBuild build64/obs-studio.sln /p:Configuration=Release /p:Platform=x64
+
+SET tag="0.1"
+"C:\Program Files (x86)\Caphyon\Advanced Installer 16.9\bin\x86\advinst.exe" /edit OssiaLive.aip /SetVersion %tag%
 
 
 
