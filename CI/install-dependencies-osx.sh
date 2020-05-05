3010d820f5eb9a055f98982f5ae1e6b77240facc
useless-stuff <diego@aimusic.co.uk>
2020-05-05 13:36:54 +0100

    refactor(bash): building bash script changed

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -35,7 +35,7 @@ if [ -d "$(brew --cellar)/swig" ]; then
 fi
 brew install https://gist.githubusercontent.com/DDRBoxman/4cada55c51803a2f963fa40ce55c9d3e/raw/572c67e908bfbc1bcb8c476ea77ea3935133f5b5/swig.rb
 
-pip install dmgbuild
+pip3 install dmgbuild
 
 export PATH=/usr/local/opt/ccache/libexec:$PATH
 ccache -s || echo "CCache is not available."
@@ -53,7 +53,7 @@ tar -xf vlc-3.0.8.tar.xz
 # Get sparkle
 hr "Downloading Sparkle framework"
 wget --quiet --retry-connrefused --waitretry=1 -O sparkle.tar.bz2 https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.bz2
-mkdir ./sparkle
+mkdir -p ./sparkle
 tar -xf ./sparkle.tar.bz2 -C ./sparkle
 sudo cp -R ./sparkle/Sparkle.framework /Library/Frameworks/Sparkle.framework
 
@@ -66,7 +66,7 @@ cd ./cef_binary_${CEF_BUILD_VERSION}_macosx64
 sed -i '.orig' '/add_subdirectory(tests\/ceftests)/d' ./CMakeLists.txt
 # target 10.11
 sed -i '.orig' s/\"10.9\"/\"10.11\"/ ./cmake/cef_variables.cmake
-mkdir build
+mkdir -p build
 cd ./build
 cmake -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_EXE_LINKER_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 ..
 make -j4
